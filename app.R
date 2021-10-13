ebba31b6aee84ff03ebb6eff8260c7af6b0867ec
ducminhngo871 <ducminhngo871@gmail.com>
2021-10-12 23:34:04 -0500

    Update app.R

--- app.R
+++ app.R
@@ -1,35 +1,60 @@
 library(shiny)
 library(tidyverse)
 library(babynames)
+library(DT)        # for table output
+library(bslib)     #for theming
+
+players_stats_by_season_full_details <- read_delim("players_stats_by_season_full_details.csv", 
+                                                   delim = ";", escape_double = FALSE, trim_ws = TRUE)
+
+players_stats_by_season_full_details %>% 
+  distinct(Player) %>% 
+  arrange(Player) %>% 
+  pull(Player)
 
 ui <- fluidPage(
-  sliderInput(inputId = "years", 
-              label = "Year Range",
-              min = 1880, 
-              max = 2019, 
-              value = c(1880,2019),
-              sep = ""),
+  theme = bs_theme(primary = "#123B60", 
+                   secondary = "#D44420", 
+                   base_font = list(font_google("Raleway"), "-apple-system", 
+                                    "BlinkMacSystemFont", "Segoe UI", "Helvetica Neue", "Arial", 
+                                    "sans-serif", "Apple Color Emoji", "Segoe UI Emoji", 
+                                    "Segoe UI Symbol"), 
+                   bootswatch = "sandstone"),
+  # Application title
+  titlePanel("NBA Basketball Players Total Points"),
+  
   textInput("name", 
             "Name", 
             value = "", 
-            placeholder = "Lisa"),
-  selectInput("sex", 
-              "Sex", 
-              choices = list(Female = "F", Male = "M")),
-  submitButton(text = "Create my plot!"),
-  plotOutput(outputId = "timeplot")
+            placeholder = "Shaquille O'Neal"),
+  
+  # Show a plot of cumulative points for the chosen player
+  # Show a table beneath
+  mainPanel(
+    plotOutput(outputId = "timeplot"),
+    dataTableOutput(outputId = "nba_table")
+  )
 )
 
 server <- function(input, output) {
+  nba_smry <- reactive(players_stats_by_season_full_details %>% 
+    filter(Player == input$name) %>% 
+    group_by(`Season Plays`) %>% 
+    summarize(season_score = sum(PTS)) %>% 
+    mutate(total_score = cumsum(season_score)))
+  
   output$timeplot <- renderPlot({
-    babynames %>% 
-      filter(name == input$name, 
-             sex == input$sex) %>% 
-      ggplot() +
-      geom_line(aes(x = year, y = n)) +
-      scale_x_continuous(limits = input$years) +
+    nba_smry() %>% 
+      ggplot(aes(x = `Season Plays`, y = total_score)) +
+      geom_line() +
+      geom_point() + 
+      labs(title = paste("Total NBA points of", input$name),
+           x = "",
+           y = "") +
       theme_minimal()
   })
+  
+  output$nba_table <- renderDataTable(nba_smry())
 }
 
 shinyApp(ui = ui, server = server)
